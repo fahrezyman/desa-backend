766c962641a0f831509bc08a5374df981035d3be
Ilham Fahrezy <ilhamfahrezyxz@gmail.com>
2024-07-07 18:45:32 +0700

    restructure

--- src/routes/criteriaRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const criteriaController = require("../controllers/criteriaController");
-
-const router = express.Router();
-
-router.post("/", criteriaController.addCriteria);
-router.get("/", criteriaController.getCriteria);
-
-module.exports = router;
--- src/routes/evaluationRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const evaluationController = require("../controllers/evaluationController");
-
-const router = express.Router();
-
-router.get("/calculate", evaluationController.calculateSAW);
-router.get("/export", evaluationController.exportPDF);
-
-module.exports = router;
